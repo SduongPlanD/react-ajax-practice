f61ffbe5fe8c66df888f02fe18ce292c75be93a7
Steve Duong <steve@Plan.D>
2019-01-21 17:25:28 -0800

    add name and text input fields, with a button to submit message

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = () => (
+    <div>
+
+        <input type="text" placeholder="Name"></input>
+        <br></br>
+        <input type="text" placeholder="Message"></input>
+        <button type="button">Send</button>
+    </div>
+)
+
+export default App
